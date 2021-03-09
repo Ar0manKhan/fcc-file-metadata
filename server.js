cd2872644145047fcb3ee18f837fe369c99536ca
Ar0manKhan <arman7739a@gmail.com>
2021-03-09 17:14:57 +0530

    Finished the project

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
 
 var app = express();
 
@@ -15,3 +16,20 @@ const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+// Configuring multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => { cb(null, "./uploads") },
+  filename: (req, file, cb) => { cb(null, file.originalname) }
+});
+const upload = multer({ storage: storage });
+
+// API to analyse file and return metadata
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  })
+})
